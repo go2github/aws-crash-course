40f7da9af3823bd9f40ffaaa7ab1508dd2c6c216
go2github <jsheik.career@gmail.com>
2022-07-30 12:26:53 +0530

    first commit

---
+++ src/main/java/aws/crash/course/AwsCrashCourseApplication.java
@@ -0,0 +1,13 @@
+package aws.crash.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCrashCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsCrashCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/aws/crash/course/AwsCrashCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package aws.crash.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsCrashCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
